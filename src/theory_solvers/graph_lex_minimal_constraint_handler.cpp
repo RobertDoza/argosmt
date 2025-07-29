426cf35ed08665adfdd4c487477719ca485223b7
Robert <dozarobert98@gmail.com>
2025-07-29 15:45:57 +0200

    Reformat graph_lex_minimal_constraint_handler.cpp

--- src/theory_solvers/graph_lex_minimal_constraint_handler.cpp
+++ src/theory_solvers/graph_lex_minimal_constraint_handler.cpp
@@ -3,145 +3,124 @@
 #define GRAPH_LEX_MIN_LOG
 
 namespace {
-    #ifdef GRAPH_LEX_MIN_LOG
+#ifdef GRAPH_LEX_MIN_LOG
     void log_message(const std::string& msg) {
         std::cout << msg << std::endl;
     }
-    #endif // GRAPH_LEX_MIN_LOG
-}
+#endif // GRAPH_LEX_MIN_LOG
+} // namespace
 
-graph_lex_minimal_constraint_handler::graph_lex_minimal_constraint_handler(csp_theory_solver * th, const expression & l_pos, const expression & l_neg)
-  :constraint_handler(th, l_pos, l_neg),
-   _common_data(&th->_graph_lex_minimal_common_data),
-   _next_to_assert(0)
-{
-    #ifdef GRAPH_LEX_MIN_LOG
+graph_lex_minimal_constraint_handler::graph_lex_minimal_constraint_handler(csp_theory_solver* th, const expression& l_pos, const expression& l_neg)
+    : constraint_handler(th, l_pos, l_neg), _common_data(&th->_graph_lex_minimal_common_data), _next_to_assert(0) {
+#ifdef GRAPH_LEX_MIN_LOG
     log_message("LexMin constraint handler constructor called");
-    #endif // GRAPH_LEX_MIN_LOG
-  _common_data->_instance_count++;
-  const expression_vector & ops = _constraint->get_operands();
-  csp_theory_solver::csp_theory_solver_data * cons_data = _theory_solver->get_theory_solver_data(_constraint);
-  for(unsigned i = 0; i < ops.size(); i++)
-    {
-      csp_theory_solver::csp_theory_solver_data * var_data = _theory_solver->add_variable(ops[i]);
-      var_data->add_variable_constraint(_constraint);
-      cons_data->add_constraint_variable(ops[i]);
+#endif // GRAPH_LEX_MIN_LOG
+    _common_data->_instance_count++;
+    const expression_vector& ops = _constraint->get_operands();
+    csp_theory_solver::csp_theory_solver_data* cons_data = _theory_solver->get_theory_solver_data(_constraint);
+    for (unsigned i = 0; i < ops.size(); i++) {
+        csp_theory_solver::csp_theory_solver_data* var_data = _theory_solver->add_variable(ops[i]);
+        var_data->add_variable_constraint(_constraint);
+        cons_data->add_constraint_variable(ops[i]);
     }
 
-  for(unsigned i = 0; i < th->_current_level; i++)
-    _trail.new_level();  
+    for (unsigned i = 0; i < th->_current_level; i++)
+        _trail.new_level();
 }
 
-
-void graph_lex_minimal_constraint_handler::new_level()
-  {
-    #ifdef GRAPH_LEX_MIN_LOG
+void graph_lex_minimal_constraint_handler::new_level() {
+#ifdef GRAPH_LEX_MIN_LOG
     log_message("LexMin constraint handler new_level() called");
-    #endif // GRAPH_LEX_MIN_LOG
+#endif // GRAPH_LEX_MIN_LOG
     _trail.new_level();
     // TODO
-  }
-  
-void graph_lex_minimal_constraint_handler::backjump(unsigned level)
-  {
-    #ifdef GRAPH_LEX_MIN_LOG
+}
+
+void graph_lex_minimal_constraint_handler::backjump(unsigned level) {
+#ifdef GRAPH_LEX_MIN_LOG
     log_message("LexMin constraint handler backjump() called");
-    #endif // GRAPH_LEX_MIN_LOG
+#endif // GRAPH_LEX_MIN_LOG
     assert(level < _trail.current_level());
     _trail.backjump(level);
     _next_to_assert = _trail.size();
-    // TODO  
-  }
-    
-void graph_lex_minimal_constraint_handler::assert_literal(const expression & l)
-  {   
-    #ifdef GRAPH_LEX_MIN_LOG
-    log_message("LexMin constraint handler assert_literal() called");
-    #endif // GRAPH_LEX_MIN_LOG
-    if(_theory_solver->get_solver().is_conflict())
-      return;
-    
-    if(_theory_solver->get_solver().get_trail().get_source_theory_solver(l) != _theory_solver ||
-       _theory_solver->get_theory_solver_data(l)->get_explanation_handler() != this)
-      {
-	_trail.push(l);
-      }
-  }
-  
-void graph_lex_minimal_constraint_handler::check_and_propagate(unsigned layer)
-  {
-    #ifdef GRAPH_LEX_MIN_LOG
-    log_message("LexMin constraint handler check_and_propagate() called");
-    #endif // GRAPH_LEX_MIN_LOG
-    if(_theory_solver->get_solver().is_conflict())
-      {
-	return;
-      }
-    
-    extended_boolean eb = _theory_solver->get_solver().get_trail().get_value(_constraint);
-    if(eb == EB_UNDEFINED)
-      return; // never happens
-    
-    if(_just_activated)
-      {
-	if(eb == EB_TRUE)
-	  propagate_weaker_from_pos_constraints();
-	else
-	  propagate_weaker_from_neg_constraints();
-	_just_activated = false;
-      }
-    
-    if(eb == EB_FALSE)
-      {
-	return;
-      }
-    
-    
     // TODO
-    
-    
-  }
-    
-void graph_lex_minimal_constraint_handler::explain_literal(const expression & l)
-  {
-    #ifdef GRAPH_LEX_MIN_LOG
-    log_message("LexMin constraint handler explain_literal() called");
-    #endif // GRAPH_LEX_MIN_LOG
-    // TODO
-  }
+}
 
-int graph_lex_minimal_constraint_handler::get_variable_value_hint(const expression & var)
-  {
-    #ifdef GRAPH_LEX_MIN_LOG
+void graph_lex_minimal_constraint_handler::assert_literal(const expression& l) {
+#ifdef GRAPH_LEX_MIN_LOG
+    log_message("LexMin constraint handler assert_literal() called");
+#endif // GRAPH_LEX_MIN_LOG
+    if (_theory_solver->get_solver().is_conflict())
+        return;
+
+    if (_theory_solver->get_solver().get_trail().get_source_theory_solver(l) != _theory_solver ||
+        _theory_solver->get_theory_solver_data(l)->get_explanation_handler() != this) {
+        _trail.push(l);
+    }
+}
+
+void graph_lex_minimal_constraint_handler::check_and_propagate(unsigned layer) {
+#ifdef GRAPH_LEX_MIN_LOG
+    log_message("LexMin constraint handler check_and_propagate() called");
+#endif // GRAPH_LEX_MIN_LOG
+    if (_theory_solver->get_solver().is_conflict()) {
+        return;
+    }
+
+    extended_boolean eb = _theory_solver->get_solver().get_trail().get_value(_constraint);
+    if (eb == EB_UNDEFINED)
+        return; // never happens
+
+    if (_just_activated) {
+        if (eb == EB_TRUE)
+            propagate_weaker_from_pos_constraints();
+        else
+            propagate_weaker_from_neg_constraints();
+        _just_activated = false;
+    }
+
+    if (eb == EB_FALSE) {
+        return;
+    }
+
+    // TODO
+}
+
+void graph_lex_minimal_constraint_handler::explain_literal(const expression& l) {
+#ifdef GRAPH_LEX_MIN_LOG
+    log_message("LexMin constraint handler explain_literal() called");
+#endif // GRAPH_LEX_MIN_LOG
+       // TODO
+}
+
+int graph_lex_minimal_constraint_handler::get_variable_value_hint(const expression& var) {
+#ifdef GRAPH_LEX_MIN_LOG
     log_message("LexMin constraint handler get_variable_value_hint() called");
-    #endif // GRAPH_LEX_MIN_LOG
+#endif // GRAPH_LEX_MIN_LOG
     // DO NOTHING
-    return 0; 
-  }
-  
-bool graph_lex_minimal_constraint_handler::is_weaker_from_pos(const expression & c)
-  {
-    #ifdef GRAPH_LEX_MIN_LOG
+    return 0;
+}
+
+bool graph_lex_minimal_constraint_handler::is_weaker_from_pos(const expression& c) {
+#ifdef GRAPH_LEX_MIN_LOG
     log_message("LexMin constraint handler is_weaker_from_pos() called");
-    #endif // GRAPH_LEX_MIN_LOG
+#endif // GRAPH_LEX_MIN_LOG
     // DO NOTHING
-    return false; 
-  }
-  
-bool graph_lex_minimal_constraint_handler::is_weaker_from_neg(const expression & c)
-  {
-    #ifdef GRAPH_LEX_MIN_LOG
+    return false;
+}
+
+bool graph_lex_minimal_constraint_handler::is_weaker_from_neg(const expression& c) {
+#ifdef GRAPH_LEX_MIN_LOG
     log_message("LexMin constraint handler is_weaker_from_neg() called");
-    #endif // GRAPH_LEX_MIN_LOG
+#endif // GRAPH_LEX_MIN_LOG
     // DO NOTHING
-    return false; 
-  }
-  
-void graph_lex_minimal_constraint_handler::check_implied()
-  {
-    #ifdef GRAPH_LEX_MIN_LOG
+    return false;
+}
+
+void graph_lex_minimal_constraint_handler::check_implied() {
+#ifdef GRAPH_LEX_MIN_LOG
     log_message("LexMin constraint handler check_implied() called");
-    #endif // GRAPH_LEX_MIN_LOG
+#endif // GRAPH_LEX_MIN_LOG
     // DO NOTHING
-  }
+}
 
